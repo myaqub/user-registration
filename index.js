e6e5868b3dd7385c55077612711b2f5bd86f43cb
yaqub mohammad <yaqubmohammad@yaqubs-imac.local>
2024-01-30 23:47:50 +0100

    updated

--- index.js
+++ index.js
@@ -1,29 +1,124 @@
-function userregistration() {   
-    
-    const userNameArray = ["yaqub", "ali", "asad", "abid", "abida"];
-    
-    const inputname=document.getElementById("username");
-    const inputpass=document.getElementById("password");
-    const btnsubmit=document.getElementById("btn");
-    const usererror=document.getElementById("usererror")
-    const passworderror=document.getElementById(password);
-    const valueToCheck=inputname.value
 
-    console.log( inputname.value,inputpass.value);
 
-    if (userNameArray.includes(valueToCheck)) {
-        usererror.textContent=valueToCheck +" already found";
-        console.log(userNameArray.length);
+    
+
+
+        // User authentication 
+        const userInput = document.getElementById('username');
+        const userError = document.getElementById('usernameError');
+
+        //password length 
+        const passwordInput = document.getElementById('password');
+        const passwordInputerror = document.getElementById('passwordError');
+
+        // confirmation password 
+        const inputconfirmationpassword=document.getElementById("confirmPassword");
+        const  inputconfirmationerror=document.getElementById("confirmPasswordError");
+
+        //Email 
+        const inputEmail =document.getElementById("email");
+
+        const   mailerror= document.getElementById( "emailError");
+
+
+        //user Name  length
+
+        userInput.addEventListener('input', function() {      
+        const userLenght = userInput.value.length;    
+      if (userLenght <5) {
+        userError.style.display ="inline";
+        userError.textContent=" User Name  must be more the 6 character ";           
+        
+      } else {
+        userError.textContent="   Available  ";
+        userError.classList.add("accept");
+        
+        
+      }
+    });
+
+    
+        // password lenght 
+        passwordInput.addEventListener('input', function() {      
+        const passwordLength = passwordInput.value.length;    
+      if (passwordLength < 8) {
+        passwordInputerror.textContent=" Password lenght must be more the 5 ";  
+        passwordInputerror.style.display="inline"     ; 
+      } else {
+        passwordInputerror.classList.add("accept");
+        passwordInputerror.textContent=" Strong Password ";
+      }
+    });
+
+
+      //  Password compare
+
+    inputconfirmationpassword.addEventListener("input", function(){       
+
+        if(inputconfirmationpassword.value !==passwordInput.value){          
+            inputconfirmationerror.textContent=" password is not matcht";
+            inputconfirmationerror.style.display="inline"    ;            
+          
+        }
+
+        else {
+            
+            inputconfirmationerror.style.display="none"     ; 
+            passwordInputerror.style.display="none"     ; 
+
+        }       
+
+    });
+
+     // Validate email
+
+            inputEmail.addEventListener("input", function(){
+
+                const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!emailRegex.test(inputEmail.value)) {
+                mailerror.textContent=" Write correct Email format";
+                mailerror.style.display="inline"    ; 
   
-  } else {
 
-    userNameArray.push(valueToCheck);
-    usererror.innerHTML="<p>  User Register Successful</p>";
-    console.log(userNameArray.length);
+             }
+
+             else {
+
+                mailerror.textContent=" goood";
+               // mailerror.style.display="none"     ; 
+
+            
+             }
+
+
+            });
+     
+            
+             
+    
+    function  register(){
+
+        const inputuserleng=userInput.value.length;
+        const  inputpasslengh=passwordInput.value.length;
+
+        if(! (inputuserleng<5)  && ! (inputpasslengh<8) ){
+
+       //     if (usernameInput.value.trim() !== '' && passwordInput.value.trim() !== '')
+            console.log("Okkkk");
+
+
+        }
+
+        else
+        {
+            console.log("not good ");
+        }
+    }
+
+
+
   
-}
-    
 
-}
 
-    
+
+
